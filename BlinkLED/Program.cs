307f84dc3c27ae2eaf7006c21e461940d62aace5
Peter Marcu <petermarcu@hotmail.com>
2017-11-01 15:00:44 -0700

    Adding BlinkLED

---
+++ BlinkLED/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using Pi.IO.GeneralPurpose;
+using Pi.IO;
+
+namespace BlinkLED
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new GpioConnectionDriver();
+            var ledPin = driver.Out(ConnectorPin.P1Pin23);
+
+            bool ledState = false;
+            while (true)
+            {
+                ledPin.Write(ledState);
+                Thread.Sleep(500);
+                ledState = ledState ? false : true;
+            }
+        }
+    }
+}
